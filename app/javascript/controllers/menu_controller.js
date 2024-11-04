d0bad269c98094c751053ee1f2f86491b9d096cb
Naoya Ito <me@n1to.com>
2024-11-04 16:29:13 +0900

    add menu overlay

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,15 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["menu"]
+  static targets = ["menu", "menubg"]
 
   open() {
     this.menuTarget.classList.remove("translate-x-full")
     this.menuTarget.classList.add("translate-x-0")
+    this.menubgTarget.classList.remove("hidden")
   }
 
   close() {
     this.menuTarget.classList.remove("translate-x-0")
     this.menuTarget.classList.add("translate-x-full")
+    this.menubgTarget.classList.add("hidden")
   }
 }
