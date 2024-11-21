bd8bc408afb3ba1ff0e224ae6bcd9eb3909a99b1
Naoya Ito <me@n1to.com>
2024-11-21 13:23:19 +0900

    update template layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,16 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Noto Sans JP"', 'sans-serif']
+      },
+      colors: {
+        lightGray: '#f2f2f2',
+      }
+    },
+  },
+  plugins: [],
 }
