7025382c7360980ab34b794e48eb4e705b5a978c
Naoya Ito <me@n1to.com>
2024-11-30 01:28:22 +0900

    update categories view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,8 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @keywords = @category.keywords.order(:sequence)
-    @pages = Page.joins(:page_keywords).where(page_keywords: { keyword_id: @keywords.pluck(:id) }).distinct.order(:updated_at).limit(10)
+    @civilian_pages = Page.joins(:page_keywords).where(genre: 1, page_keywords: { keyword_id: @keywords.pluck(:id) }).distinct.order(:updated_at).limit(5)
+    @government_pages = Page.joins(:page_keywords).where(genre: 2, page_keywords: { keyword_id: @keywords.pluck(:id) }).distinct.order(:updated_at).limit(5)
+    @page_posts = PagePost.joins(:page).where(page_id: @civilian_pages.map(&:id) + @government_pages.map(&:id)).order(created_at: :desc).limit(5)
   end
 end
