7293025318db37398e839110e0c8a630145a471a
Naoya Ito <me@n1to.com>
2024-12-01 01:02:20 +0900

    update toast design

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,6 @@ class CategoriesController < ApplicationController
     @keywords = @category.keywords.order(:sequence)
     @civilian_pages = Page.joins(:page_keywords).where(genre: 1, page_keywords: { keyword_id: @keywords.pluck(:id) }).distinct.order(updated_at: :desc).limit(5)
     @government_pages = Page.joins(:page_keywords).where(genre: 2, page_keywords: { keyword_id: @keywords.pluck(:id) }).distinct.order(updated_at: :desc).limit(5)
-    @page_posts = PagePost.joins(:page).where(page_id: @civilian_pages.map(&:id) + @government_pages.map(&:id)).order(created_at: :desc).limit(5)
+    @page_posts = PagePost.joins(:page).where(page_id: @civilian_pages.map(&:id) + @government_pages.map(&:id)).order(created_at: :desc).limit(5) # TODO
   end
 end
