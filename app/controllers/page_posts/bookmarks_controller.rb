16c2eb37db5fd94d8de3a90c9950887ecf3087b3
Naoya Ito <me@n1to.com>
2024-12-19 17:37:19 +0900

    impl page_post bookmark

--- app/controllers/page_posts/bookmarks_controller.rb
+++ app/controllers/page_posts/bookmarks_controller.rb
@@ -4,17 +4,18 @@ class PagePosts::BookmarksController < ApplicationController
     bookmark = current_user.page_post_bookmarks.build(page_post:)
     if bookmark.save
       flash[:info] = t("flash.create")
-      redirect_to session.delete(:return_to)
+      redirect_to page_post
     else
       flash[:error] = t("flash.failed")
-      redirect_to session.delete(:return_to)
+      redirect_to page_post
     end
   end
 
   def destroy
+    page_post = PagePost.find(params[:page_post_id])
     bookmark = current_user.page_post_bookmarks.find_by(page_post_id: params[:page_post_id])
     bookmark.destroy
     flash[:info] = t("flash.destroy")
-    redirect_to session.delete(:return_to)
+    redirect_to page_post
   end
 end
