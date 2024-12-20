a47ea684a41d97a209d4d7fee2dba066679f3b2e
Naoya Ito <me@n1to.com>
2024-12-20 13:53:24 +0900

    use turbo stream for page_post bookmark

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   def index
     redirect_to signup_setup_path if current_user && !current_user.setup?
 
-    session[:return_to] = request.url
     @categories = Category.order(:sequence)
     @page_posts = PagePost.order(created_at: :desc).limit(5)
     @official_posts = OfficialPost.order(created_at: :desc).limit(5)
--- app/controllers/page_posts/bookmarks_controller.rb
+++ app/controllers/page_posts/bookmarks_controller.rb
@@ -1,21 +1,30 @@
 class PagePosts::BookmarksController < ApplicationController
   def create
-    page_post = PagePost.find(params[:page_post_id])
-    bookmark = current_user.page_post_bookmarks.build(page_post:)
-    if bookmark.save
-      flash[:info] = t("flash.create")
-      redirect_to page_post
+    @page_post = PagePost.find(params[:page_post_id])
+    @bookmark = current_user.page_post_bookmarks.build(page_post: @page_post)
+    if @bookmark.save
+      flash.now[:info] = t("flash.create")
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to @page_post }
+      end
     else
-      flash[:error] = t("flash.failed")
-      redirect_to page_post
+      flash.now[:error] = t("flash.failed")
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to @page_post }
+      end
     end
   end
 
   def destroy
-    page_post = PagePost.find(params[:page_post_id])
-    bookmark = current_user.page_post_bookmarks.find_by(page_post_id: params[:page_post_id])
-    bookmark.destroy
-    flash[:info] = t("flash.destroy")
-    redirect_to page_post
+    @page_post = PagePost.find(params[:page_post_id])
+    @bookmark = current_user.page_post_bookmarks.find_by(page_post_id: params[:page_post_id])
+    @bookmark.destroy
+    flash.now[:info] = t("flash.destroy")
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to @page_post }
+    end
   end
 end
