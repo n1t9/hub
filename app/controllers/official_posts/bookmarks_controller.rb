e5fabf1ec40582515e9f4bffb9880dc397f5491a
Naoya Ito <me@n1to.com>
2024-12-20 15:03:18 +0900

    impl official_post bookmark with turbo

--- app/controllers/official_posts/bookmarks_controller.rb
+++ app/controllers/official_posts/bookmarks_controller.rb
@@ -1,20 +1,30 @@
 class OfficialPosts::BookmarksController < ApplicationController
   def create
-    official_post = OfficialPost.find(params[:official_post_id])
-    bookmark = current_user.official_post_bookmarks.build(official_post:)
-    if bookmark.save
-      flash[:info] = t("flash.create")
-      redirect_to session.delete(:return_to)
+    @official_post = OfficialPost.find(params[:official_post_id])
+    @bookmark = current_user.official_post_bookmarks.build(official_post: @official_post)
+    if @bookmark.save
+      flash.now[:info] = t("flash.create")
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to @official_post }
+      end
     else
-      flash[:error] = t("flash.failed")
-      redirect_to session.delete(:return_to)
+      flash.now[:error] = t("flash.failed")
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to @official_post }
+      end
     end
   end
 
   def destroy
-    bookmark = current_user.official_post_bookmarks.find_by(official_post_id: params[:official_post_id])
-    bookmark.destroy
-    flash[:info] = t("flash.destroy")
-    redirect_to session.delete(:return_to)
+    @official_post = OfficialPost.find(params[:official_post_id])
+    @bookmark = current_user.official_post_bookmarks.find_by(official_post_id: params[:official_post_id])
+    @bookmark.destroy
+    flash.now[:info] = t("flash.destroy")
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to @official_post }
+    end
   end
 end
