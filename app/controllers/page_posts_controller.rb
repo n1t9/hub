3dd58e7c747bbcabf4da6cca935447c84c7f1bf6
Naoya Ito <me@n1to.com>
2024-12-21 23:38:32 +0900

    impl page_posts#index

--- app/controllers/page_posts_controller.rb
+++ app/controllers/page_posts_controller.rb
@@ -1,4 +1,13 @@
 class PagePostsController < ApplicationController
+  def index
+    if params[:category_id]
+      @category = Category.find(params[:category_id])
+      @page_posts = PagePost.includes(:page).where(pages: { category_id: @category.id }).order(created_at: :desc).page(params[:page]).per(10)
+    else
+      @page_posts = PagePost.order(created_at: :desc).page(params[:page]).per(10)
+    end
+  end
+
   def show
     @page_post = PagePost.find(params[:id])
   end
