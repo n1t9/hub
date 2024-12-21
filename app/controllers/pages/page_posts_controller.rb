8e23af69f4f30d258b5a6fe2419fed00341f6634
Naoya Ito <me@n1to.com>
2024-12-21 16:55:47 +0900

    impl update and delete page_post

--- app/controllers/pages/page_posts_controller.rb
+++ app/controllers/pages/page_posts_controller.rb
@@ -20,6 +20,37 @@ class Pages::PagePostsController < ApplicationController
     end
   end
 
+  def edit
+    @page = Page.find(params[:page_id])
+    return redirect_to page_path(params[:page_id]) unless current_user&.page_manager?(@page)
+
+    @page_post = @page.page_posts.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:page_id])
+    return redirect_to page_path(params[:page_id]) unless current_user&.page_manager?(@page)
+
+    @page_post = @page.page_posts.find(params[:id])
+    if @page_post.update(page_post_params)
+      flash[:success] = t("flash.update")
+      redirect_to page_path(@page)
+    else
+      flash[:alert] = @page_post.errors.full_messages
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @page = Page.find(params[:page_id])
+    return redirect_to page_path(params[:page_id]) unless current_user&.page_manager?(@page)
+
+    @page_post = @page.page_posts.find(params[:id])
+    @page_post.destroy
+    flash[:success] = t("flash.destroy")
+    redirect_to page_path(@page)
+  end
+
   private
 
   def page_post_params
