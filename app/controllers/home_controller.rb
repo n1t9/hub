397109e702c9048bf32488a85650131a94f1a37b
Naoya Ito <me@n1to.com>
2024-12-23 14:33:56 +0900

    add following_pages and following_page_posts to home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,9 @@ class HomeController < ApplicationController
     @categories = Category.order(:sequence)
     @page_posts = PagePost.order(created_at: :desc).limit(5)
     @official_posts = OfficialPost.order(created_at: :desc).limit(5)
+    if current_user
+      @following_pages = current_user.following_pages.order(updated_at: :desc).limit(5)
+      @following_page_posts = PagePost.where(page_id: current_user.following_pages.pluck(:id)).order(created_at: :desc).limit(5)
+    end
   end
 end
